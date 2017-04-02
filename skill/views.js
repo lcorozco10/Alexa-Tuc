477c5f02fb18142cf0d4b36c7da2d04fda2b8da0
Moilex90 <moises.aburto.git@gmail.com>
2017-04-02 13:38:45 -0600

    Change views - Change views for alexa in SSML mode

--- skill/views.js
+++ skill/views.js
@@ -4,25 +4,25 @@ const views = (function views() {
   return {
     Intent: {
       Balance: {
-        say: 'Your Card ID is checking now',
+        say: "Your Card <say-as interpret-as='spell-out'>Id</say-as> is checking now <break time='300ms'/>",
       },
       totalBalance : {
-        ask : "your quantity in your card is {totalBalance}, Do you want asking other Card Id?"
+        ask : "your balance is {totalBalance} <break time='300ms'/> Do you want asking other Card <say-as interpret-as='spell-out'>Id</say-as>?"
       },    
       askCardId : {
-        ask : "your card id is invalid, Can you give me a valid card id?"
+        ask : "your card <say-as interpret-as='spell-out'>Id</say-as> is invalid <break time='300ms'/> Can you give me a valid card <say-as interpret-as='spell-out'>Id</say-as>?"
       },
       askAgainCardId : {
-        ask : "Please, give me your Id"
+        ask : "Please <break time='300ms'/> give me your <say-as interpret-as='spell-out'>Id</say-as>"
       },
       serverError: {
-        tell: 'Sorry,I can not help you in this time',
+        tell: "Sorry<break time='100ms'/>I can not help you in this time",
       },
       didNotUnderStand : {
-        ask : "I did not understand what are you saying , you can ask me the card id again?"
+        ask : "I did not understand what are you saying <break time='300ms'/> Can you ask me the card <say-as interpret-as='spell-out'>Id</say-as> again?"
       },
       exit: {
-        tell: 'Ok, thank you!',
+        tell: "Ok <break time='300ms'/> thank you!",
       },
       Help: {
         say: 'Some help text here.',
